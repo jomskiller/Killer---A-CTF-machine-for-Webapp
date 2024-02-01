ade69f5d677d73ac3674102a17b084550b64f063
jomskiller <145160254+jomskiller@users.noreply.github.com>
2024-02-01 16:34:11 +0800

    Create victim1.php

---
+++ victim1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Victim1.php Page</title>
+    <style>
+        body {
+            background-color: #8b0000; /* Dark red background */
+            color: #fff; /* White text color */
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 50px;
+            margin: 0;
+        }
+
+        h1 {
+            font-size: 3em; /* Larger font size */
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>This room is a dead end. GRRRRRRRRR</h1>
+    
+</body>
+</html>
